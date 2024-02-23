0eaa715951d65cf6e09a562eb8359c003a8944dd
Vignesh Sankar Iyer <vignesh.sankar.iyer.1999@gmail.com>
2024-02-23 14:31:04 +0800

    set up tailwind

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -5,14 +5,14 @@ function Product({ title, price, description, category, image }) {
 
 export default function ProductList() {
   return (
-    <main>
-      <h1>Product List</h1>
+    <main className="p-8 bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600 mb-4">Product List</h1>
 
       {/* show category here */}
-      <ul></ul>
+      <ul className="list-disc pl-5 mb-4"></ul>
 
       {/* show products here */}
-      <ul></ul>
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></ul>
     </main>
   );
 }
